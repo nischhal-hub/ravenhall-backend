edaa0f5a5384c4de0666b1301e9ee148d931e7b5
nischal <nischalbh51@gmail.com>
2026-05-09 09:44:24 +0545

    fix: add refresh token to login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -37,7 +37,7 @@ export const login = async (
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
-    sendSuccess(res, { user, accessToken }, "Login successful");
+    sendSuccess(res, { user, accessToken, refreshToken }, "Login successful");
   } catch (error) {
     next(error);
   }
